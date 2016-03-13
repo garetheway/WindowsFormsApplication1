5caa4474aacf38e220af84dfeb137eaeff704c95
garetheway <garetheway@gmail.com>
2016-03-13 04:07:42 +0000

    Visual via dynamic text boxes
    Added a visual element to the schedule via dynamic textboxes.

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -15,17 +15,78 @@ namespace WindowsFormsApplication1
         public Form1(Population pop, WorkOrderCollection workOrders)
         {
             InitializeComponent();
+            List<TextBox> prodLine1 = new List<TextBox>();
+            List<TextBox> prodLine2 = new List<TextBox>();
+            
+            // SET UP PRODUCTION LINE 1
+            for (int i = 0; i < pop.ProdLine1.Count; i++)
+            {
+                prodLine1.Add(new TextBox());
+                prodLine1[i].Location = new System.Drawing.Point(i * 155, 25);
+                prodLine1[i].Size = new System.Drawing.Size(150,90);
+                prodLine1[i].Multiline = true;
+                prodLine1[i].Enabled = false;
+                this.Controls.Add(prodLine1[i]);
+            }
 
-            // Work order IDS
+            // SET UP PRODUCTION LINE 2
+            for (int i = 0; i < pop.ProdLine2.Count; i++)
+            {
+                prodLine2.Add(new TextBox());
+                prodLine2[i].Location = new System.Drawing.Point(i * 155, 150);
+                prodLine2[i].Size = new System.Drawing.Size(150, 90);
+                prodLine2[i].Multiline = true;
+                prodLine2[i].Enabled = false;
+                this.Controls.Add(prodLine2[i]);
+            }
+
+            // ALLOCATION OF LINE 1
             foreach (int myInt in pop.ProdLine1)
             {
-                prodLine1txt.AppendText(workOrders.orders[myInt].WorkOrderID.ToString() + "\t");
+                //prodLine1txt.AppendText(workOrders.orders[myInt].WorkOrderID.ToString() + "\t");
+                foreach (TextBox wo in prodLine1)
+                {
+                    //if (workOrders.orders[myInt].CompletionDate > workOrders.orders[myInt].DueDate)
+                    //{
+                    //    wo.BackColor = Color.Red;
+                    //}
+
+                    if (wo.Text == String.Empty)
+                    {
+                        wo.AppendText("ID: " + workOrders.orders[myInt].WorkOrderID.ToString() + Environment.NewLine + "Customer: " + workOrders.orders[myInt].Customer + Environment.NewLine + "Product: " + workOrders.orders[myInt].Product + Environment.NewLine + "Qty: " + workOrders.orders[myInt].Quantity + Environment.NewLine + "Due: " + workOrders.orders[myInt].DueDate.Date.ToString("dd/MM/yyyy") +Environment.NewLine + "CD: " + workOrders.orders[myInt].CompletionDate.Date.ToString("dd/MM/yyyy"));
+                        break;
+                    }
+                }
             }
 
+            // ALLOCATION OF LINE 2
             foreach (int myInt in pop.ProdLine2)
             {
-                prodLine2txt.AppendText(workOrders.orders[myInt].WorkOrderID.ToString() + "\t");
+                //prodLine1txt.AppendText(workOrders.orders[myInt].WorkOrderID.ToString() + "\t");
+                foreach (TextBox wo in prodLine2)
+                {
+                    //if (workOrders.orders[myInt].CompletionDate > workOrders.orders[myInt].DueDate)
+                    //{
+                    //    wo.BackColor = Color.Red;
+                    //}
+
+                    if (wo.Text == String.Empty)
+                    {
+                        wo.AppendText("ID: " + workOrders.orders[myInt].WorkOrderID.ToString() + Environment.NewLine + "Customer: " + workOrders.orders[myInt].Customer + Environment.NewLine + "Product: " + workOrders.orders[myInt].Product + Environment.NewLine + "Qty: " + workOrders.orders[myInt].Quantity + Environment.NewLine + "Due: " + workOrders.orders[myInt].DueDate.Date.ToString("dd/MM/yyyy") + Environment.NewLine + "CD: " + workOrders.orders[myInt].CompletionDate.Date.ToString("dd/MM/yyyy"));
+                        break;
+                    }
+                }
             }
+
+
+            //prodLine1txt.Enabled = false;
+            foreach (int myInt in pop.ProdLine2)
+            {
+             //   prodLine2txt.AppendText(workOrders.orders[myInt].WorkOrderID.ToString() + "\t");
+            }
+            //prodLine2txt.Enabled = false;
+
+
 
         }
     }
--- WindowsFormsApplication1/WorkOrderCollection.cs
+++ WindowsFormsApplication1/WorkOrderCollection.cs
@@ -12,14 +12,14 @@ namespace WindowsFormsApplication1
 
         public void AssociateWorkOrders()
         {
-            orders.Add(new WorkOrder("100", "ProductA", "Joe", new DateTime(2016, 3, 09), 2000));
-            orders.Add(new WorkOrder("101", "ProductC", "John", new DateTime(2016, 3, 10), 5000));
-            orders.Add(new WorkOrder("102", "ProductC", "George", new DateTime(2016, 3, 11), 3000));
-            orders.Add(new WorkOrder("103", "ProductC", "Josh", new DateTime(2016, 3, 13), 10000));
-            orders.Add(new WorkOrder("104", "ProductB", "Gareth", new DateTime(2016, 3, 10), 5000));
-            orders.Add(new WorkOrder("105", "ProductA", "David", new DateTime(2016, 3, 11), 3000));
+            orders.Add(new WorkOrder("100", "ProductA", "Joe", new DateTime(2016, 3, 13), 2000));
+            orders.Add(new WorkOrder("101", "ProductC", "John", new DateTime(2016, 3, 14), 5000));
+            orders.Add(new WorkOrder("102", "ProductC", "George", new DateTime(2016, 3, 13), 3000));
+            orders.Add(new WorkOrder("103", "ProductC", "Josh", new DateTime(2016, 3, 15), 10000));
+            orders.Add(new WorkOrder("104", "ProductB", "Gareth", new DateTime(2016, 3, 13), 5000));
+            orders.Add(new WorkOrder("105", "ProductA", "David", new DateTime(2016, 3, 14), 3000));
             orders.Add(new WorkOrder("106", "ProductB", "Tom", new DateTime(2016, 3, 20), 7000));
-            orders.Add(new WorkOrder("107", "ProductB", "Eric", new DateTime(2016, 3, 10), 3000));
+            orders.Add(new WorkOrder("107", "ProductB", "Eric", new DateTime(2016, 3, 18), 3000));
             orders.Add(new WorkOrder("108", "ProductA", "Ross", new DateTime(2016, 3, 29), 10000));
             orders.Add(new WorkOrder("109", "ProductD", "Ellie", new DateTime(2016, 3, 25), 10000));
         }
